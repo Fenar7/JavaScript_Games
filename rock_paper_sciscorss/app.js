a7b59c51ec3f2e96402474299fde00f909690d57
Fenar Khan <fenarkhan@gmail.com>
2023-12-19 15:38:45 +0530

    5

--- rock_paper_sciscorss/app.js
+++ rock_paper_sciscorss/app.js
@@ -1,3 +1,28 @@
-const computerChoice = document.getElementById('computer-choice')
-const yourChoice = document.getElementById('your-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('your-choice')
 const result = document.getElementById('result')
+let computerChoice
+let userChoice
+
+const possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    genrateComputerChoice()
+}))
+
+function genrateComputerChoice(){
+    const randomNumber = Math.floor( Math.random() * 3) + 1
+    console.log(randomNumber)
+
+    if(randomNumber==1){
+        computerChoice = 'rock'
+    }
+    else if(randomNumber==2){
+        computerChoice = 'scissors'
+    }
+    else{
+        computerChoice = 'paper'
+    }
+}
