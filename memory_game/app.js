11bf7a89cb8675a7fa7ba180fe488fe017574184
Fenar Khan <fenarkhan@gmail.com>
2023-12-21 17:59:42 +0530

    jdfkjsdfkj

--- memory_game/app.js
+++ memory_game/app.js
@@ -69,12 +69,18 @@ console.log(cardArray)
 const gridDisplay = document.querySelector('#grid')
 
 function createBoard(){
-    for(let i=0;i<10;i++){
+    for(let i=0;i<cardArray.length;i++){
         const card = document.createElement('img')
         card.setAttribute('src','images/blank.png')
         card.setAttribute('data-id',i)
+        card.addEventListener('click',flipCard)
+        gridDisplay.appendChild(card)
         console.log(card,i)
     }
 }
 
 createBoard()
+
+function flipCard(){
+
+}
